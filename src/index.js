a931cd0c0c0368409ddbd6749ad43ca32d7ac4f9
Abbas <30557675+AbbasKDG@users.noreply.github.com>
2019-05-07 13:34:20 -0600

    React

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import TodoList from "./TodoList";
+//tells our bundling tool such as webpack to put all our pieces together in one bundle
+var destination = document.querySelector("#container");
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <div>
+    <TodoList />
+  </div>,
+  destination
+); // what is being import and the destination of the import
---
+++ src/test.js
@@ -0,0 +1,3 @@
+const user = { fN: "A", lN: "M" };
+const element = <h1>{fN(user)}</h1>;
+console.log(element);
